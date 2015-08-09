17335433d9cd4b7930bc25cc60fdae666677880e
skydreaming <skydreamings@qq.com>
2015-08-09 17:05:45 +0800

    用户协议界面

--- app/src/main/java/com/quark/jianzhidaren/AgreementActivity.java
+++ app/src/main/java/com/quark/jianzhidaren/AgreementActivity.java
@@ -2,25 +2,51 @@ package com.quark.jianzhidaren;
 
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.parttime.base.WithTitleActivity;
 import com.qingmu.jianzhidaren.R;
 
-public class AgreementActivity extends BaseActivity {
+public class AgreementActivity extends WithTitleActivity {
+
+	@Override
+	protected ViewGroup getLeftWrapper() {
+		return null;
+	}
+
+	@Override
+	protected ViewGroup getRightWrapper() {
+		return null;
+	}
+
+	@Override
+	protected TextView getCenter() {
+		return null;
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_agreement);
-		setBackButton();
-		setTopTitle("注册协议");
+		super.onCreate(savedInstanceState);
+//		setBackButton();
+//		setTopTitle("注册协议");
 		WebView webView = (WebView) findViewById(R.id.set);
 		WebSettings webSettings = webView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
 		webView.requestFocus();
 		webView.setHorizontalScrollBarEnabled(false);
 		webView.setVerticalScrollBarEnabled(false);
-		webView.loadUrl("file:///android_asset/agreement.html");
+		webView.loadUrl("file:///android_asset/cagreement.html");
+	}
+
+	@Override
+	protected void initViews() {
+		super.initViews();
+		center(R.string.agreement);
+		left(ImageButton.class, R.drawable.nav_btn_back);
 	}
 }
